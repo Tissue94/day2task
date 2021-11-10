4cccebcf04a84423e1d35d1cdc9ce91e97a7d70f
Tissue94 <tissue2979@naver.com>
2021-11-10 09:18:32 +0900

    주석 추가

--- heightRank.js
+++ heightRank.js
@@ -3,6 +3,7 @@
 
 const readLine = require('readline-sync')
 
+//키 150 ~ 190 사이 임의의 4개 인덱스 생성
 const heightArry = []
 heightArry.push(parseInt(Math.random() * 40) + 150)
 heightArry.push(parseInt(Math.random() * 40) + 150)
@@ -11,6 +12,7 @@ heightArry.push(parseInt(Math.random() * 40) + 150)
 
 console.log(heightArry)
 
+//값에 따라 인덱스 순서 정렬 버블정렬????
 for (let i = 0; i < heightArry.length; i++) {
     let swap
     for (let j = 0; j < heightArry.length - 1 - i; j++) {
@@ -23,14 +25,16 @@ for (let i = 0; i < heightArry.length; i++) {
 }
 console.log(heightArry)
 
+//키 / 등수 입력받기
 console.log(`당신의 키를 입력하시오`)
 
 const yourHeight = parseInt(readLine.prompt())
 
-console.log(`당신이 생각하는 키 등수를 입력하시오`)
+console.log(`당신이 생각하는 키 등수를 맞춰보세요`)
 
 let rank = parseInt(readLine.prompt())
 
+// 키 / 등수에 따라 출력
 if( yourHeight < heightArry[0]){
     if(rank === 5)
     {
